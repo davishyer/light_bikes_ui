9c8104753e6f2d210895988b9829c2a857a9f819
Davis Hyer <dhyer@instructure.com>
2018-10-11 14:18:19 -0600

    stop polling when viewing a deleted game

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const serverUrl = 'http://light-bikes.inseng.net'
 class App extends Component {
   state = {
     games: [],
+    onDeadGame: false
   }
 
   componentDidMount() {
@@ -21,7 +22,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <div className="App-header" onClick={this.handleClick}>
+        <div className="App-header" onClick={this.handleClick.bind(this)}>
           Light Bikes!
         </div>
         <div className="App-games">
@@ -33,7 +34,10 @@ class App extends Component {
   }
 
   triggerFetch() {
-    setTimeout(() => this.fetchGames(), fetchInterval)
+    const { onDeadGame } = this.state
+    if (!onDeadGame) {
+      setTimeout(() => this.fetchGames(), fetchInterval)
+    }
   }
 
   fetchGames() {
@@ -43,6 +47,12 @@ class App extends Component {
       })
       this.triggerFetch()
     }).catch(e => {
+      // if the game isn't found, no need to continue polling for it
+      if (e.response.status === 404) {
+        this.setState({
+          onDeadGame: true
+        })
+      }
       this.triggerFetch()
     })
   }
@@ -54,6 +64,11 @@ class App extends Component {
   handleClick() {
     if (window.location.hash !== "") {
       window.location.hash = ""
+
+      // reset dead game tracking when navigating home
+      this.setState({
+        onDeadGame: false
+      }, this.triggerFetch)
     }
   }
 }
